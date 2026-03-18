3054c3e313ee69877640d9fb335c58786d3010dc
sueszli <jabaryyahya@gmail.com>
2026-03-19 00:42:52 +0100

    up

--- leaderboard/bench_exojit.py
+++ leaderboard/bench_exojit.py
@@ -1,6 +1,13 @@
+# /// script
+# requires-python = "==3.14.*"
+# dependencies = [
+#   "exojit @ git+https://github.com/sueszli/exojit.git",
+#   "numpy",  # just for ffi
+# ]
+# ///
+
 import numpy as np
 from exo import *
-from exo.stdlib.scheduling import divide_loop, fission, rename, reorder_loops, simplify, unroll_loop
 
 from exojit.main import compile_jit
 
@@ -14,15 +21,8 @@ def matmul(C: f32[128, 128] @ DRAM, A: f32[128, 128] @ DRAM, B: f32[128, 128] @ 
                 C[i, j] += A[i, k] * B[k, j]
 
 
-opt = rename(matmul, "opt")
-opt = fission(opt, opt.find("for k in _: _").before(), n_lifts=2)
-opt = reorder_loops(opt, "j k")
-opt = divide_loop(opt, "j #1", 4, ["jo", "ji"], perfect=True)
-opt = unroll_loop(opt, "ji")
-opt = simplify(opt)
-
-N = 128
-A, B = np.random.randn(N, N).astype(np.float32), np.random.randn(N, N).astype(np.float32)
-C = np.zeros((N, N), dtype=np.float32)
-compile_jit(opt)[opt.name()](C, A, B)
-assert np.allclose(C, A @ B, atol=0.5)
+A = np.random.randn(128, 128).astype(np.float32)
+B = np.random.randn(128, 128).astype(np.float32)
+C = np.zeros((128, 128), np.float32)
+compile_jit(matmul)[matmul.name()](C, A, B)
+print(C)
