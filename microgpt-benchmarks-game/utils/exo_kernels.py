28f6f8cede110d7bdeb97ae2c943aade3f6702d8
sueszli <jabaryyahya@gmail.com>
2026-03-20 21:45:26 +0100

    up

--- microgpt-benchmarks-game/utils/exo_kernels.py
+++ microgpt-benchmarks-game/utils/exo_kernels.py
@@ -52,7 +52,6 @@ def matmul_left_t(M: size, N: size, K: size, out: f64[N, K] @ DRAM, x: f64[M, N]
 
 @proc
 def softmax(M: size, N: size, x: f64[M, N] @ DRAM):
-    # in-place softmax over last dimension
     for i in seq(0, M):
         mx: f64 @ Stack
         sum_val: f64 @ Stack
@@ -139,7 +138,6 @@ def cross_entropy_bwd(M: size, N: size, probs: f64[M, N] @ DRAM, target_ids: siz
 
 @proc
 def adam(N: size, param: f64[N] @ DRAM, grad: f64[N] @ DRAM, m: f64[N] @ DRAM, v: f64[N] @ DRAM, lr: f64[1] @ DRAM, beta1_t: f64[1] @ DRAM, beta2_t: f64[1] @ DRAM):
-    # adam step over flat buffer (with bias correction from caller)
     inv_b1: f64 @ Stack
     inv_b2: f64 @ Stack
     inv_beta1_t: f64 @ Stack
@@ -163,3 +161,78 @@ def adam(N: size, param: f64[N] @ DRAM, grad: f64[N] @ DRAM, m: f64[N] @ DRAM, v
         param[i] = param[i] - lr[0] * m_hat / (sqrt(v_hat) + 1e-8)
         m[i] = m_val
         v[i] = v_val
+
+
+@proc
+def dot_product_attention(S: size, H: size, D: size, out: f64[H, S, D] @ DRAM, q: f64[H, S, D] @ DRAM, k: f64[H, S, D] @ DRAM, v: f64[H, S, D] @ DRAM, attn_w: f64[H, S, S] @ DRAM, scale: f64[1] @ DRAM):
+    for h in seq(0, H):
+        for t in seq(0, S):
+            for s in seq(0, S):
+                logit: f64 @ Stack
+                if s > t:
+                    logit = -1e10
+                else:
+                    logit = 0.0
+                    for d in seq(0, D):
+                        logit += q[h, t, d] * k[h, s, d]
+                    logit = logit * scale[0]
+                attn_w[h, t, s] = logit
+
+    for h in seq(0, H):
+        for t in seq(0, S):
+            mx: f64 @ Stack
+            sum_val: f64 @ Stack
+            mx = -1e10
+            for s in seq(0, S):
+                mx = select(mx, attn_w[h, t, s], attn_w[h, t, s], mx)
+            sum_val = 0.0
+            for s in seq(0, S):
+                attn_w[h, t, s] = expf(attn_w[h, t, s] - mx)
+                sum_val += attn_w[h, t, s]
+            for s in seq(0, S):
+                attn_w[h, t, s] = attn_w[h, t, s] / sum_val
+
+    for h in seq(0, H):
+        for t in seq(0, S):
+            for d in seq(0, D):
+                acc: f64 @ Stack
+                acc = 0.0
+                for s in seq(0, S):
+                    acc += attn_w[h, t, s] * v[h, s, d]
+                out[h, t, d] = acc
+
+
+@proc
+def dot_product_attention_bwd(S: size, H: size, D: size, dq: f64[H, S, D] @ DRAM, dk: f64[H, S, D] @ DRAM, dv: f64[H, S, D] @ DRAM, dout: f64[H, S, D] @ DRAM, q: f64[H, S, D] @ DRAM, k: f64[H, S, D] @ DRAM, v: f64[H, S, D] @ DRAM, attn_w: f64[H, S, S] @ DRAM, scale: f64[1] @ DRAM):
+    attn_tmp: f64[S] @ Stack
+    for h in seq(0, H):
+        for t in seq(0, S):
+            for d in seq(0, D):
+                dv[h, t, d] = 0.0
+                dq[h, t, d] = 0.0
+        for s in seq(0, S):
+            for d in seq(0, D):
+                dk[h, s, d] = 0.0
+
+    for h in seq(0, H):
+        for t in seq(0, S):
+            dot: f64 @ Stack
+            dot = 0.0
+            for s in seq(0, S):
+                dattn_w: f64 @ Stack
+                dattn_w = 0.0
+                for d in seq(0, D):
+                    dattn_w += dout[h, t, d] * v[h, s, d]
+                    dv[h, s, d] += attn_w[h, t, s] * dout[h, t, d]
+                attn_tmp[s] = dattn_w
+                dot += dattn_w * attn_w[h, t, s]
+
+            for s in seq(0, S):
+                dlogit: f64 @ Stack
+                if s > t:
+                    dlogit = 0.0
+                else:
+                    dlogit = attn_w[h, t, s] * (attn_tmp[s] - dot) * scale[0]
+                for d in seq(0, D):
+                    dq[h, t, d] += dlogit * k[h, s, d]
+                    dk[h, s, d] += dlogit * q[h, t, d]
